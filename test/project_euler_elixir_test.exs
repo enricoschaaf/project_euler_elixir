088789c453cc1ea6f2163daad65c05eebef91fe8
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-11-10 16:41:18 +0100

    Project 2

--- test/project_euler_elixir_test.exs
+++ test/project_euler_elixir_test.exs
@@ -2,7 +2,7 @@ defmodule ProjectEulerElixirTest do
   use ExUnit.Case
   doctest ProjectEulerElixir
 
-  test "greets the world" do
-    assert ProjectEulerElixir.hello() == :world
+  test "By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms." do
+    assert ProjectEulerElixir.main(4_000_000) == 4_613_732
   end
 end
