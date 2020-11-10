503898444ad3558bb075889b8bcdafc0b3330e96
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-11-10 15:43:40 +0100

    Problem 1

--- lib/project_euler_elixir.ex
+++ lib/project_euler_elixir.ex
@@ -4,15 +4,17 @@ defmodule ProjectEulerElixir do
   """
 
   @doc """
-  Hello world.
+  Problem 1
+  Multiples of 3 and 5
 
-  ## Examples
+  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-      iex> ProjectEulerElixir.hello()
-      :world
-
+  Find the sum of all the multiples of 3 or 5 below 1000.
   """
-  def hello do
-    :world
+
+  def main(n) do
+    0..(n - 1)
+    |> Enum.filter(fn x -> rem(x, 3) == 0 or rem(x, 5) == 0 end)
+    |> Enum.sum()
   end
 end
--- test/project_euler_elixir_test.exs
+++ test/project_euler_elixir_test.exs
@@ -2,7 +2,7 @@ defmodule ProjectEulerElixirTest do
   use ExUnit.Case
   doctest ProjectEulerElixir
 
-  test "greets the world" do
-    assert ProjectEulerElixir.hello() == :world
+  test "Find the sum of all the multiples of 3 or 5 below 1000." do
+    assert ProjectEulerElixir.main(1000) == 233_168
   end
 end
